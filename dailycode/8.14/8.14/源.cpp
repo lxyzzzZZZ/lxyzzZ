ed028ba4c011a88d1670805451327816fd82c9d6
lxyzzzZZZ <1909980451@qq.com>
2019-08-14 09:56:27 +0800

    8.14

---
+++ dailycode/8.14/8.14/源.cpp
@@ -0,0 +1,93 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Person
+//{
+//public:
+//	Person(const char* name = "peter")
+//		: _name(name)
+//	{
+//		cout << "Person()" << endl;
+//	}
+//	Person(const Person& p)
+//		: _name(p._name)
+//	{
+//		cout << "Person(const Person& p)" << endl;
+//	}
+//	Person& operator=(const Person& p)
+//	{
+//		cout << "Person operator=(const Person& p)" << endl;
+//		if (this != &p)
+//			_name = p._name;
+//		return *this;
+//	}
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//public:
+//	Student(const char* name, int num)
+//		: Person(name)
+//		, _num(num)
+//	{
+//		cout << "Student()" << endl;
+//	}
+//	Student(const Student& s)
+//		: Person(s)
+//		, _num(s._num)
+//	{
+//		cout << "Student(const Student& s)" << endl;
+//	}
+//	Student& operator = (const Student& s)
+//	{
+//		cout << "Student& operator= (const Student& s)" << endl;
+//		if (this != &s)
+//		{
+//			Person::operator =(s);
+//			_num = s._num;
+//		}
+//		return *this;
+//	}
+//	~Student()
+//	{
+//		cout << "~Student()" << endl;
+//	}
+//protected:
+//	int _num; //学号
+//};
+//void Test()
+//{
+//	Student s1("jack", 18);
+//	Student s2(s1);
+//	Student s3("rose", 17);
+//	s1 = s3;
+//}
+//class Student;
+//class Person
+//{
+//public:
+//	friend void Display(const Person& p, const Student& s);
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//protected:
+//	int _stuNum; // 学号
+//};
+//void Display(const Person& p, const Student& s) {
+//	cout << p._name << endl;
+//	cout << s._stuNum << endl;
+//}
+//void main()
+//{
+//	Person p;
+//	Student s;
+//	Display(p, s);
+//}
